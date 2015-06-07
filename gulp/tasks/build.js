46462ed701962fda8f1f566a551d67d2b9c1016a
radical-edo <radical.3do@gmail.com>
2015-06-07 10:43:10 +0200

    vendor configuration

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -6,6 +6,15 @@ var inject = require('gulp-inject');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
+var bowerFiles = require('main-bower-files');
+var concat = require('gulp-concat');
+var _ = require('lodash');
+
+function buildVendor () {
+    return gulp.src(bowerFiles())
+    .pipe(concat('vendor.js'))
+    .pipe(gulp.dest(config.paths.dest))
+}
 
 function buildApp () {
   return browserify(config.paths.js, {
@@ -18,8 +27,15 @@ function buildApp () {
   .pipe(gulp.dest(config.paths.dest));
 }
 
+var INJECT_OPTIONS = {
+  ignorePath: config.paths.dest,
+  addRootSlash: false,
+};
+
 module.exports = function() {
   return gulp.src(config.paths.index)
       .pipe(inject(buildApp(), { ignorePath: config.paths.dest, addRootSlash: false }))
+      .pipe(inject(buildVendor(), _.merge({ name: 'vendor'}, INJECT_OPTIONS)))
+      .pipe(inject(buildApp(), INJECT_OPTIONS))
       .pipe(gulp.dest(config.paths.dest));
 };
